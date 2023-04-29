1464379ecc869157975bf9e99c50be3596477414
jorsh08 <jorgeverdugo3005@gmail.com>
2023-04-28 19:53:08 -0600

    nombre arreglados

--- Vistas/Notificaciones/ListadoNotificaciones.js
+++ Vistas/Notificaciones/ListadoNotificaciones.js
@@ -6,7 +6,7 @@ const ListadoNotificaciones= () => {
     
     const notificaciones = [{id:1, tipo:1, desc:'Notificación 1', descExt:'Descripción notificación 1'}, {id:2, tipo:2, desc:'Notificación 2', descExt:'Descripción notificación 2'},{id:3, tipo:3, desc:'Notificación 3', descExt:'Descripción notificación 3'},{id:4, tipo:4, desc:'Notificación 4', descExt:'Descripción notificación 4'},{id:5, tipo:5, desc:'Notificación 5', descExt:'Descripción notificación 5'}]
     
-    const iconos = [require('../../assets/notificaciones/Evento/Evento-Recordatorio-C.png'),require('../../assets/notificaciones/Evento/Evento-Favorito-C.png'),require('../../assets/notificaciones/Evento/Evento-Seguidor-C.png'), require('../../assets/notificaciones/Notificaciones/Notificacion-Admin-C.png'),require('../../assets/notificaciones/Notificaciones/Notificacion-General-C.png')];
+    //const iconos = [require('../../assets/notificaciones/Evento/Evento-Recordatorio-C.png'),require('../../assets/notificaciones/Evento/Evento-Favorito-C.png'),require('../../assets/notificaciones/Evento/Evento-Seguidor-C.png'), require('../../assets/notificaciones/Notificaciones/Notificacion-Admin-C.png'),require('../../assets/notificaciones/Notificaciones/Notificacion-General-C.png')];
 
   return (
         <View style={{width: '100%', height: '100%', marginTop: 10}}>
@@ -17,7 +17,6 @@ const ListadoNotificaciones= () => {
                 <Pressable key={element.id}>
                     <View style={{flexDirection:'row',marginHorizontal:'10%',height:90, width:'100%'}}>
                         <View style={{flex:1.5, alignContent:'center',alignItems:'center',justifyContent:'center'}}>
-                            <Image style={styles.imagenEvento} source={iconos[element.tipo-1]}/>
                         </View>
                         <View style={{flex:7, justifyContent:'center'}}>
                             <Text style={styles.tituloNoti}>
